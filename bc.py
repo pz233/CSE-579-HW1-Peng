f615f1b230ffea45ad9ed2dd4f078fc4354639b8
pz233 <152771527+pz233@users.noreply.github.com>
2024-10-11 12:08:13 -0700

    Update bc.py

--- bc.py
+++ bc.py
@@ -16,11 +16,26 @@ def simulate_policy_bc(env, policy, expert_data, num_epochs=500, episode_length=
         ## TODO Students
         np.random.shuffle(idxs)
         running_loss = 0.0
+        expert_flattened = {'observations': [],
+                        'actions': []}
+
+        for index in idxs:
+          for k in expert_flattened.keys():
+            expert_flattened[k].append(expert_data[index][k])
+
+        for k in expert_flattened.keys():
+          expert_flattened[k] = np.concatenate(expert_flattened[k])
+          
         for i in range(num_batches):
             optimizer.zero_grad()
             #========== TODO: start ==========
             # Fill in your behavior cloning implementation here
-            
+            batch_observation = expert_flattened['observations'][i*batch_size:(i+1)*batch_size]
+            batch_action = expert_flattened['actions'][i*batch_size:(i+1)*batch_size]
+
+            action_pred = policy(batch_observation)
+            criterion = nn.NLLLoss()
+            loss = criterion(action_pred, batch_action) 
 
             #========== TODO: end ==========
             loss.backward()
